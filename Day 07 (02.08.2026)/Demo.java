c5430fcae076fdb89075d2b53aced73eaca4bff5
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-08-02 14:03:15 +0530

    Your message

--- Day 07 (02.08.2026)/Demo.java
+++ Day 07 (02.08.2026)/Demo.java
@@ -1,37 +1,51 @@
 class A{
 	int a;
-	A(){
-		System.out.println("A()");
-	}
-	A(int i){
-		System.out.println("A(int)");
-	}
-	A(int i, int j){
-		System.out.println("A(int,int)");
+	public void print(){
+		System.out.println("print of A ");
 	}
 }
 class B extends A{ 
 	int b;
-	B(){
-		super();
-		System.out.println("B()");
-	}
-	B(int i){
-		super(i);
-		System.out.println("B(int)");
-	}
-	B(int i, int j){
-		super(i,j);
-		System.out.println("B(int,int)");
+	public void print(){
+		System.out.println("print of B ");
 	}
 }
-
+class C extends B{ 
+	int c;
+	public void print(){
+		System.out.println("print of C ");
+	}
+}
+class D extends B{ 
+	int d;
+	public void print(){
+		System.out.println("print of D ");
+	}
+}
 class Demo{
 	public static void main(String args[]){
+		A a1=new A();
+		A a2=new B();
+		A a3=new C();
+		A a4=new D();
+		
 		B b1=new B();
-		System.out.println("--------------");
-		B b2=new B(100);
-		System.out.println("--------------");
-		B b3=new B(100,200);
+		B b2=new C();
+		B b3=new D();
+		
+		C c1=new C();
+		D d1=new D();
+		
+		a1.print(); //
+		a2.print();	//
+		a3.print();
+		a4.print();
+		b1.print();
+		b2.print();
+		b3.print();
+		c1.print();
+		d1.print(); 
+		
+		
 	}
 }
