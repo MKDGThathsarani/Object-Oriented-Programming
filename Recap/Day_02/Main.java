89cc839fb82591dff781ed69fc0426aeb18e157b
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-24 13:45:37 +0530

    Add new updates and activities

--- Recap/Day_02/Main.java
+++ Recap/Day_02/Main.java
@@ -1,1 +1,59 @@
-
+class BankAccount{
+	private String accoutNumber;
+	private String accoutHolder;
+	private double balance;
+	
+	public BankAccount(String accoutNumber,String accoutHolder,double balance){
+		this.accoutNumber = accoutNumber;
+		this.accoutHolder = accoutHolder;
+		this.balance = balance;
+		}
+		
+		//Getters
+		public String getAccountNumber(){
+			return accoutNumber;
+			}
+		public String getAccoutHolder(){
+			return accoutHolder;
+			}
+		public double getBalance(){
+			return balance;
+			}
+			
+		//Setters
+		public void setAccountHolder(String accoutHolder){
+			this.accoutHolder = accoutHolder;
+			}
+			
+		//Deposit Method
+		public void deposit(double amount){
+			if (amount > 0){
+				balance += amount;
+				System.out.println("Deposited: " + amount);
+				}
+			}
+		
+		//Withdrow
+		public void withdraw(double amount){
+			if (amount > 0 && amount <= balance){
+				balance -= amount;
+				System.out.println("Withdrown: " + amount);
+				}else{
+					System.out.println("Insufficient balance!");
+					}
+			}
+			
+		public void display(){
+			System.out.println("Account: " + accountNumber);
+			System.out.println("Holder: " + accoutHolder);
+			System.out.println("Balance: " + balance);
+			}
+	}
+	
+	public class Main{
+		public static void main(String args[]){
+			BankAccount acc = new BankAccount("ACC001","Kmal",100000);
+			
+			System.out.println();
+			}
+		} 
